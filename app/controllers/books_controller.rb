13460d74024b9d62a74713088841a6f571ff74ba
QVH201 <quachhoa25052001@gmail.com>
2023-02-02 10:08:22 +0000

    Add detail book

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -1,5 +1,5 @@
 class BooksController < ApplicationController
-  before_action :set_book, only: [:show, :edit, :update, :destroy]
+  before_action :set_book, only: [:show, :edit, :update, :destroy, :detail] 
     before_action :authenticate_user!
     
   # GET /books or /books.json
